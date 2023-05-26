51685ce5430e562509c5b80a9fb5a292601743b1
Joseph Lam <joseph.lam.18@ucl.ac.uk>
2023-05-26 14:15:15 +0100

    Update 4_describe_syn_sample.r

--- 4_describe_syn_sample.r
+++ 4_describe_syn_sample.r
@@ -109,6 +109,69 @@ gold <- read.csv("fake_testing.csv")
 gold <- data.frame(gold)
 
 #compare in other ways
+gold$ethgroup <- as.factor(gold$ethgroup)
+gold$maternal_agecat <- as.factor(gold$maternal_agecat)
+gold_dist_nototal <- table(gold$ethgroup, gold$maternal_agecat, useNA = "always")
+gold_dist <- addmargins(gold_dist_nototal)
+gold_prop <- prop.table(gold_dist, margin = 1)
+
+# prop difference
+prop_diff <- list()
+prop_diff_precent <- list()
+
+for (i in 1:length(prop)){
+  prop_diff[[i]] <- round(gold_prop - prop[[i]], 4)
+  prop_diff_percent[[i]] <- prop_diff[[i]] * 100
+}
+
+# Set the desired level of confidence
+confidence_level <- 0.95
+
+# Create an empty table to store the averaged values and confidence intervals
+averaged_table <- matrix(0, nrow = 5, ncol = 5)
+confidence_intervals <- matrix(0, nrow = 5, ncol = 5)
+
+# Iterate over each value in the table
+for (i in 1:5) {
+  for (j in 1:5) {
+    sum_value <- 0
+    squared_sum <- 0
+    
+    # Calculate the sum and squared sum across the 200 tables
+    for (k in 1:200) {
+      value <- prop_diff_percent[[k]][i, j]  # proportion difference in percent
+      sum_value <- sum_value + value
+      squared_sum <- squared_sum + value^2
+    }
+    
+    # Calculate the mean and standard deviation
+    mean_value <- sum_value / 200
+    variance <- (squared_sum - sum_value^2/200) / 199
+    standard_deviation <- sqrt(variance)
+    
+    # Calculate the standard error
+    standard_error <- standard_deviation / sqrt(200)
+    
+    # Calculate the critical value based on the desired confidence level
+    critical_value <- qnorm((1 + confidence_level) / 2)
+    
+    # Calculate the confidence interval
+    lower_bound <- mean_value - (critical_value * standard_error)
+    upper_bound <- mean_value + (critical_value * standard_error)
+    
+    # Populate the averaged value and confidence interval in the respective tables
+    averaged_table[i, j] <- mean_value
+    confidence_intervals[i, j] <- paste0("[", round(lower_bound, 3), ", ", round(upper_bound, 3), "]")
+  }
+}
+
+# Print the averaged table and confidence intervals together
+for (i in 1:5) {
+  for (j in 1:5) {
+    cat("Average:", round(averaged_table[i, j], 3), "CI:", confidence_intervals[i, j], "\n")
+  }
+}
+
 
 
 
